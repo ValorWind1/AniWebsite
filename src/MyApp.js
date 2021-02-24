45674bd306a4e70bee6c97d9940f3e7c8c8d6cfb
ValorWind1 <skyjdani@gmail.com>
2021-02-24 02:05:42 -0500

    draft apis

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,6 +1,24 @@
-import React from "react"
+import React,{useState , useEffect} from "react"
+import axios from "axios"
 
 const MyApp =()=> {
+
+    const [manga , setManga] = useState([])
+
+    const headers = {
+          'x-rapidapi-key': 'ecf2cd78aamshe6b1fc38f7bf795p1052cajsnca6e52e07661',
+          'x-rapidapi-host': 'jikan1.p.rapidapi.com'
+        }
+      
+
+    useEffect(() => {
+        axios.get('https://jikan1.p.rapidapi.com/manga/2/characters' , {headers}).then(response => {
+            setManga(response)
+            console.log(manga)
+        })
+    })
+   
+    
 
     return (
         <div>
